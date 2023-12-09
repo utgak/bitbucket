0a3a621a0234a49c827e6154129224846eb1388e
usr <degtyaryov.2002@mail.ru>
2023-12-09 06:38:38 +0300

    update specs

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -23,8 +23,14 @@ RSpec.describe GildedRose do
       expect(items[0].sell_in).to eq -1
     end
 
-    it "not increasing quality after 50" do
-      items = [Item.new("Brie", -1, 50)]
+    it "not increasing quality after 50 when + 1" do
+      items = [Item.new("Brie", 1, 49)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 50
+    end
+
+    it "not increasing quality after 50 when + 2" do
+      items = [Item.new("Brie", -1, 49)]
       GildedRose.new(items).update_quality()
       expect(items[0].quality).to eq 50
     end
